5080489fd955beb6c6effa8c5949f9e905c710ec
tiago <t@t.com>
2021-11-25 22:49:54 +0000

    refs #1 - update

--- src/repository/decorators.ts
+++ src/repository/decorators.ts
@@ -3,13 +3,23 @@ import {RepositoryKeys} from "./constants";
 
 /**
  * Defines a class as a repository (makes it injectable)
+ * and forces an instantiation over any other possible with that key
+ *
+ * @param {any[]} [props] optional props to be passed to {@link injectable}
+ *
+ * @see injectable
+ * with args:
+ *  - category: {@link RepositoryKeys.REPO};
+ *  - singleton: true;
+ *  - force: true;
+ *  - args: {@param props}
  *
  * @decorator repository
- * @namespace Decorators
- * @memberOf Model
+ * @namespace decorators
+ * @memberOf repository
  */
-export function repository(){
+export function repository(...props: any[]){
     return (original: Function) => {
-        return injectable(RepositoryKeys.REPO, true)(original);
+        return injectable(RepositoryKeys.REPO, true, true, ...props)(original);
     }
 }
