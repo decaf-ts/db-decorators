3f67d07b5b8a0364f564a4a99f558867ceaf1e70
Tiago <venceslau.tiago@gmail.com>
2024-11-07 03:47:55 +0000

    refs #1 - update

--- src/interfaces/Contextual.ts
+++ src/interfaces/Contextual.ts
@@ -1,7 +1,7 @@
 import { OperationKeys } from "../operations";
-import { Constructor, Model } from "@decaf-ts/decorator-validation";
+import { Model } from "@decaf-ts/decorator-validation";
 import { Context } from "../repository";
 
 export interface Contextual<M extends Model> {
-  context(operation: OperationKeys, model: Constructor<M>): Promise<Context<M>>;
+  context(operation: OperationKeys, ...args: any[]): Promise<Context<M>>;
 }
