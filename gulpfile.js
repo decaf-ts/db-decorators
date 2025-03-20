352535e549d57245c9f9bdb39b8db6438ee91504
tvenceslau <tiago.venceslau@pdmfc.com>
2025-03-20 21:29:14 +0000

    DECAF-90 - updates gulp build to stop index bug

--- gulpfile.js
+++ gulpfile.js
@@ -31,7 +31,8 @@ function patchFiles() {
     };
   };
 
-  return series(doPatch("lib"), doPatch("dist"));
+  return series(doPatch("lib"));
+  // return series(doPatch("lib"), doPatch("dist"));
 }
 
 function getWebpackConfig(isESM, isDev) {
@@ -132,7 +133,6 @@ function exportDefault(isDev, mode) {
         stream.dts.pipe(dest(destPath)),
         stream.js
           .pipe(gulpIf(!isDev, uglify()))
-          .pipe(gulpIf(isDev, sourcemaps.write()))
           .pipe(
             gulpIf(
               mode === "commonjs",
@@ -141,6 +141,7 @@ function exportDefault(isDev, mode) {
               })
             )
           )
+          .pipe(gulpIf(isDev, sourcemaps.write()))
           .pipe(
             gulpIf(
               mode === "commonjs",
@@ -223,7 +224,7 @@ export const dev = series(
 
 export const prod = series(
   parallel(
-    series(exportDefault(true, "commonjs"), exportDefault(true, "es2022")),
+    series(exportDefault(false, "commonjs"), exportDefault(false, "es2022")),
     exportESMDist(false),
     exportJSDist(false)
   ),
