65a2b1130a1b2116f997ee97bccf023e3578a862
tvenceslau <tiago.venceslau@pdmfc.com>
2025-05-04 12:16:54 +0100

    DECAF-125 decorator extensions

---
+++ src/repository/constants.ts
@@ -0,0 +1,10 @@
+import { RepositoryFlags } from "./types";
+
+export const DefaultRepositoryFlags: Omit<RepositoryFlags, "timestamp"> = {
+  parentContext: undefined,
+  childContexts: [],
+  callArgs: [],
+  writeOperation: false,
+  affectedTables: [],
+  operation: undefined,
+};
