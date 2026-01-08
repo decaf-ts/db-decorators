71981339cc717de56dd76b385e36585935c99d3d
tvenceslau <venceslau.tiago@gmail.com>
2026-01-08 21:50:51 +0000

    patch - DECAF-206 better ctx-no-ci - after release preparation

--- src/interfaces/Contextual.ts
+++ src/interfaces/Contextual.ts
@@ -24,6 +24,7 @@ export interface Contextual<C extends Context<any> = Context<any>> {
    */
   context(
     operation:
+      | ((...args: any[]) => any)
       | OperationKeys.CREATE
       | OperationKeys.READ
       | OperationKeys.UPDATE
